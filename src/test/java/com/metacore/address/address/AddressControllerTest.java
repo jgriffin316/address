276a724089e80439cfd03077b0b403fada9dda8e
John Griffin <jgriffin316@gmail.com>
2018-05-12 22:13:32 -0400

    Added initial asciidoc documentation

--- src/test/java/com/metacore/address/address/AddressControllerTest.java
+++ src/test/java/com/metacore/address/address/AddressControllerTest.java
@@ -3,16 +3,33 @@ package com.metacore.address.address;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.headers.HeaderDocumentation.headerWithName;
+import static org.springframework.restdocs.headers.HeaderDocumentation.responseHeaders;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessResponse;
+import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.requestFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.document;
+import static org.springframework.restdocs.webtestclient.WebTestClientRestDocumentation.documentationConfiguration;
 
 import java.util.Optional;
 
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.JUnitRestDocumentation;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.reactive.server.WebTestClient.RequestBodySpec;
@@ -23,13 +40,25 @@ import com.metacore.address.address.resource.AddressResourceRequest;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest(AddressController.class)
+@ExtendWith({ RestDocumentationExtension.class, SpringExtension.class })
 public class AddressControllerTest {
 
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
   @Autowired
+  private ApplicationContext context;
+
   WebTestClient client;
 
   @MockBean
   AddressService addressService;
+
+  @Before
+  public void setUp() {
+    client = WebTestClient.bindToApplicationContext(context).configureClient()
+        .filter(documentationConfiguration(restDocumentation)).build();
+  }
 
   @Test
   public void create_noRequestBody_expectBadRequestError() throws Exception {
@@ -106,6 +135,34 @@ public class AddressControllerTest {
   public void delete_goodId_expect202() {
     client.delete().uri("/v1/address/goodId").accept(MediaType.APPLICATION_JSON_UTF8).exchange().expectStatus()
         .isAccepted().expectBody().isEmpty();
+  }
+
+  @Test
+  public void post_documentation() {
+    AddressResourceRequest request = createAddressResourceRequest();
+    Address expected = createAddress(request);
+
+    when(addressService.create(any(Address.class))).thenReturn(expected);
+
+    client.post().uri("/v1/address").accept(MediaType.APPLICATION_JSON_UTF8).body(BodyInserters.fromObject(request))
+        .exchange().expectStatus().isCreated().expectBody()
+        .consumeWith(document("post-address", preprocessRequest(prettyPrint()), preprocessResponse(prettyPrint()),
+            requestFields(fieldWithPath("number").description("The address street number"),
+                fieldWithPath("unit").description("The address unit number"),
+                fieldWithPath("street").description("The address street name"),
+                fieldWithPath("city").description("The address city"),
+                fieldWithPath("zip").description("The address postal number"),
+                fieldWithPath("state").description("The address state or province"),
+                fieldWithPath("country").description("The address country")),
+            responseFields(fieldWithPath("id").description("The unique id of the address"),
+                fieldWithPath("number").description("The address street number"),
+                fieldWithPath("unit").description("The address unit number"),
+                fieldWithPath("street").description("The address street name"),
+                fieldWithPath("city").description("The address city"),
+                fieldWithPath("zip").description("The address postal number"),
+                fieldWithPath("state").description("The address state or province"),
+                fieldWithPath("country").description("The address country")),
+            responseHeaders(headerWithName("location").description("Locaton of created resource"))));
   }
 
   private static AddressResourceRequest createAddressResourceRequest() {
