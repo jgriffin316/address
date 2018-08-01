76d1f76ecac632106e63e72d8eb4f01728ed769f
John Griffin <jgriffin316@gmail.com>
2018-07-31 22:56:34 -0400

    Added incomplete updates to search controller class

--- src/main/java/com/metacore/address/AddressApplication.java
+++ src/main/java/com/metacore/address/AddressApplication.java
@@ -2,8 +2,10 @@ package com.metacore.address;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class AddressApplication {
 
   public static void main(String[] args) {
--- src/main/java/com/metacore/address/search/SearchController.java
+++ src/main/java/com/metacore/address/search/SearchController.java
@@ -1,10 +1,70 @@
 package com.metacore.address.search;
 
+import static java.util.stream.Collectors.toList;
+import static org.springframework.http.MediaType.APPLICATION_JSON_UTF8;
+import static org.springframework.http.ResponseEntity.badRequest;
+
+import java.text.ParseException;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/v1/search")
-public class SearchController {
+import com.metacore.address.address.AddressAssembler;
+import com.metacore.address.address.resource.AddressResourceError;
+import com.metacore.address.address.resource.AddressResourceResponse;
+import com.metacore.address.domain.Address;
 
+@RestController
+@RequestMapping("/v1/address")
+public class SearchController {
+  SearchParser queryParser;
+
+  @Autowired
+  SearchService searchService;
+
+  public SearchController() {
+    queryParser = new SearchParser();
+    queryParser.setPropertyList(Arrays.asList("number", "unit", "street", "city", "zip", "state", "country"));
+  }
+
+  @GetMapping("")
+  public ResponseEntity<List<AddressResourceResponse>> search(
+      @RequestParam(name = "query", required = true) String query) throws ParseException, Exception {
+    return new ResponseEntity<>(retrieveResponse(query), HttpStatus.OK);
+  }
+
+  @ExceptionHandler(RuntimeException.class)
+  ResponseEntity<AddressResourceResponse> exceptionHandler(RuntimeException ex) {
+    return sendErrorResponse(toAddressResourceErrorList(ex));
+  }
+
+  ResponseEntity<AddressResourceResponse> sendErrorResponse(List<AddressResourceError> errors) {
+    AddressResourceResponse body = new AddressResourceResponse();
+    body.errors = errors;
+    return badRequest().contentType(APPLICATION_JSON_UTF8).body(body);
+  }
+
+  List<AddressResourceError> toAddressResourceErrorList(RuntimeException error) {
+    return Arrays.asList(createAddressResourceError(error));
+  }
+
+  AddressResourceError createAddressResourceError(RuntimeException error) {
+    return new AddressResourceError("query", error.getMessage());
+  }
+
+  List<AddressResourceResponse> retrieveResponse(String query) throws ParseException, Exception {
+    return searchService.search(queryParser.parse(query)).map(it -> toResponse(it)).collect(toList());
+  }
+
+  AddressResourceResponse toResponse(Address address) {
+    return AddressAssembler.toResponseObject(address);
+  }
 }
